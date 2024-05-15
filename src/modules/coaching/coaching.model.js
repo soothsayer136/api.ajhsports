6c33c7cb8209106ca2126b525b65a7f86aa1b59c
Bikrum Katwal <bikrum.katwal@gmail.com>
2024-05-15 21:14:44 +1000

    Fixed

--- src/modules/coaching/coaching.model.js
+++ src/modules/coaching/coaching.model.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const slug = require('mongoose-slug-updater');
+const slug = require("mongoose-slug-updater");
 mongoose.plugin(slug);
 
 const Price = {
   name: { type: String },
   private: { type: Number },
-  group: { type: Number }
+  group: { type: Number },
 };
 
 const coachingLessonSchema = new Schema(
@@ -14,10 +14,10 @@ const coachingLessonSchema = new Schema(
     image: { type: String },
     title: { type: String, required: true },
     description: { type: String, required: true },
-    slug: { type: String, unique: true, index: true, slug: 'title' },
+    slug: { type: String, unique: true, index: true, slug: "title" },
     price: { type: [Price] },
     time: { type: String },
-    interval: { type: Number},
+    interval: { type: String },
     location: { type: String },
     is_deleted: { type: Boolean, default: false },
     is_active: { type: Boolean, default: true },
@@ -25,4 +25,4 @@ const coachingLessonSchema = new Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model('CoachingLesson', coachingLessonSchema);
+module.exports = mongoose.model("CoachingLesson", coachingLessonSchema);
